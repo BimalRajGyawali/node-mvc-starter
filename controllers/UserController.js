cda44a53c6044c6adb15c48974aee51a6f0f317e
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2022-05-20 23:14:00 +0545

    refactor: changes class to async function

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,11 @@
 const userService = require("../services/UserService")
 
-class UserController{
-    getAll(req, res){
-        
-        userService.getAll((err)=>{
-            res.json(err)
-        }, (users)=>{
-            console.log(users);
-            res.render("users", {users: users})
-            
-        })
-    }
-    findById(req, res){
-        userService.findById(req.params.id, (err)=>{
-            res.json(err)
-        }, (user)=>{
-            res.render("user", {user: user})
-        })
-    }
+async function getAllUsers(){
+    return userService.getAllUsers();
 }
 
-module.exports = new UserController()
+async function getById(id){
+    return userService.getById(id);
+}
+
+module.exports = {getAllUsers, getById}
