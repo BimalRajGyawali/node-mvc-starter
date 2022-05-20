39b909f4c72c4a8f633d6475afc3d0504fa57a9b
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2022-05-20 23:35:57 +0545

    refactor: manages spaces

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express")
-const app = express()
-
-
 const userRouter = require("./routers/UserRouter")
+
+const app = express()
 
 app.set('view engine', 'ejs');
 
-
 app.use("/users", userRouter)
+
 app.listen(8000)
